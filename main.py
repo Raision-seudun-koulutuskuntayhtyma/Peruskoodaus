13fadb54aede3cf9b708b84ba9704c9457fe122b
Mika Vainio <koti.mika.vainio@gmail.com>
2021-08-26 15:17:28 +0300

    Lisätty TODO-merkintä

--- main.py
+++ main.py
@@ -3,17 +3,16 @@
 # Modulien ja kirjastojen lataukset
 import hetutarkistus as ht
 
+# Silmukka, jossa ollaan kunnes on syötetty järkevä henkilötunnus
 hetu_jarkeva = False
-
 while hetu_jarkeva == False:
     # Kysytään käyttäjältä henkilötunnus
     kysytty_hetu = input('Anna henkilötunnus: ')
 
     # Tarkistetaan, että hetu on oikean pituinen
     pituus_oikein = ht.tarkista_pituus(kysytty_hetu)
-
-
     if pituus_oikein == True:
+
         # Tarkistetaan onko hetu oikein
         try:
             oli_oikein = ht.tarkista_hetu(kysytty_hetu)
@@ -24,6 +23,7 @@ while hetu_jarkeva == False:
                 print('Sukupuoli:', ht.selvita_sukupuoli(kysytty_hetu))
                 print('Hän on syntynyt', ht.syntymapaiva(kysytty_hetu))
                 hetu_jarkeva = True
+                # TODO: lisää tähän ikäfunktio
             else:
                 print('Henkilötunnus väärin, tarkista!')
                 hetu_jarkeva = False
