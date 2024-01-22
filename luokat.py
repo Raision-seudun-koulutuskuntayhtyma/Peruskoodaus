6fa42e56d6f5a0de0d752daa8a9b7348a0fce1f3
Tuomas Suutari <tuomas@nepnep.net>
2024-01-22 11:04:45 +0200

    Esimerkki luokista

---
+++ luokat.py
@@ -0,0 +1,46 @@
+class Eläin:
+    def __init__(self, nimi, paino_kg):
+        self.nimi = nimi
+        self.paino_kg = paino_kg
+
+    def tervehdi(self):
+        print("Moi! Olen", self.nimi)
+
+    def nuku(self):
+        print("Zzzz..")
+
+
+class Koira(Eläin):
+    def tervehdi(self):
+        print("Hau! Hau!")
+
+
+class Kissa(Eläin):
+    def tervehdi(self):
+        print("Miau!")
+
+    def nuku(self):
+        print("Purr...")
+
+
+# Luodaan kaksi oliota (object), koira ja kissa:
+koira = Koira("Musti", 12)
+kissa = Kissa("Kisu", 4)
+
+# lista-muuttuja alla on list-luokan olio
+lista = [1, 2, 3]
+
+
+# Attribuuttien käyttäminen onnistuu pisteen avulla
+print("Koiran nimi:", koira.nimi)
+print("Kissan nimi:", kissa.nimi)
+print("Koiran paino:", koira.paino_kg, "kg")
+print("Kissan paino:", kissa.paino_kg, "kg")
+
+print("Kutsutaan tervehdi-metodia koirasta ja kissasta:")
+koira.tervehdi()
+kissa.tervehdi()
+
+print("Kokeillaan nuku-metodia:")
+koira.nuku()
+kissa.nuku()
